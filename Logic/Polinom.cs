91a965489c09b8fa9e2748979c1bee77f71f1b8f
Islam-Mamayeu <Islam_Mamayeu@epam.com>
2015-12-17 19:20:29 +0300

    Full version without tests

--- Logic/Polinom.cs
+++ Logic/Polinom.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
-    class Polinom 
+    public class Polinom 
     {
         private int x;
         private double[] coefficients;
@@ -48,6 +48,23 @@ namespace Logic
             Array.Copy(coefficients, this.coefficients, coefficients.Length);
         }
 
+
+        private void Simplify()
+        {
+            int pos = 0;
+            while (pos < coefficients.Length && coefficients[pos] == 0)
+                pos++;
+            if (pos == coefficients.Length)
+                coefficients = new double[] { 0 };
+            else if (pos > 0)
+            {
+                double[] array = new double[coefficients.Length - pos];
+                Array.Copy(coefficients, pos, array, 0, coefficients.Length - pos);
+                coefficients = array;
+            }
+        }
+
+
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
@@ -74,55 +91,32 @@ namespace Logic
 
         public static Polinom operator +(Polinom p, double value)
         {
+            Polinom result = new Polinom();
+            result.coefficients = new double[p.coefficients.Length];
             for (int i = 0; i < p.coefficients.Length; i++)
-            {
-                p.coefficients[i] += value;
-            }
-            return p;
+                result.coefficients[i] = i == p.coefficients.Length - 1 ? p.coefficients[i] + value : p.coefficients[i];
+            return result;
         }
-
         public static Polinom operator -(Polinom p, double value)
         {
-            p += (-value);
-            return p;
+            return p + (-value);
         }
-
         public static Polinom operator +(Polinom p1, Polinom p2)
         {
-            if (p1 != null || p2 != null)
-            {
-                Polinom result;
-                int maxLength = Math.Max(p1.coefficients.Length, p2.coefficients.Length);
-                //result.coefficients = new double[maxLength];
-
-                if (maxLength == p1.coefficients.Length)
-                {
-                    result = new Polinom(p1.coefficients);
-                    for (int i = 0; i < result.coefficients.Length; i++)
-                    {
-                        result += p2.coefficients[i];
-                    }
-                }
-                else
-                {
-                    result = new Polinom(p2.coefficients);
-                    for (int i = 0; i < result.coefficients.Length; i++)
-                    {
-                        result += p1.coefficients[i];
-                    }
-
-
-                }
-                return result;
-            }
-            return null;
+            Polinom result = new Polinom();
+            result.coefficients = new double[Math.Max(p1.coefficients.Length, p2.coefficients.Length)];
+            int diff = p1.coefficients.Length - p2.coefficients.Length;
+            for (int i = 0; i < result.coefficients.Length; i++)
+                result.coefficients[i] = diff > 0 ?
+                                            diff - i > 0 ? p1.coefficients[i] : p1.coefficients[i] + p2.coefficients[i - diff] :
+                                            diff + i < 0 ? p2.coefficients[i] : p1.coefficients[diff + i] + p2.coefficients[i];
+            result.Simplify();
+            return result;
         }
-
         public static Polinom operator -(Polinom p1, Polinom p2)
         {
             return p1 + (p2 * (-1));
         }
-
         public static Polinom operator *(Polinom p, double value)
         {
             Polinom result = new Polinom();
@@ -136,7 +130,25 @@ namespace Logic
             }
             return result;
         }
+        public static bool operator !=(Polinom p1, Polinom p2)
+        {
+            if (p1 == p2) return false;
+            return true;
+        }
+        public static bool operator ==(Polinom p1, Polinom p2)
+        {
+            if (p1.coefficients.Length == p2.coefficients.Length)
+            {
+                for (int i = 0; i < p1.coefficients.Length; i++)
+                {
+                    if (p1.coefficients[i] != p1.coefficients[i]) return false;
 
+                }
+                return true;
+            }
+
+            return false;
+        }
         public double Calc(double value)
         {
             double sum = 0;
--- TestPolinom/TestPolinomClass.cs
+++ TestPolinom/TestPolinomClass.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Logic;
+
 
 namespace TestPolinom
 {
-    class TestPolinomClass
+    [TestFixture]
+    public class TestPolinomClass
     {
-    }
+        [Test]
+        public void PolinomEqualsTest(Polinom p1,Polinom p2)
+        {
+
+            double[] mas = { 4, 2, 6, 4, -3, 0, 4 };
+            double[] mas2 = { 4, 2, 6, 4, -3, 0, 4 };
+            p1 = new Polinom(mas);
+            p2 = new Polinom(mas2);
+
+            Assert.AreEqual(true, p1==p2);
+
+        }
+
+        public bool PolinomNotEqualsTest(Polinom p1, Polinom p2)
+        {
+            return false;
+        }
+
+
+        }
 }
